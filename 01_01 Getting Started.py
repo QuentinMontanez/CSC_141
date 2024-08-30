f72df7f4c07af218beebbb6e94243ede86530863
OmniVegito <121777449+OmniVegito@users.noreply.github.com>
2024-08-30 10:15:20 -0400

    Update and rename 01_01 Getting Started to 01_01 Getting Started.py

---
+++ 01_01 Getting Started.py
@@ -0,0 +1,4 @@
+#I'm really interested into programming because I've always wanted to expand my ability of technology.
+#I'm also interested into coding because I want to know how to manage things in the program. 
+#I'm interested in development because I'll set a goal to evolove my capablilites in tech.
+#
