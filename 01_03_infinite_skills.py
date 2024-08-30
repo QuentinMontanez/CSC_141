ac9b18ac1d2d44a6c995e0adebf8490dc2eccfa0
OmniVegito <121777449+OmniVegito@users.noreply.github.com>
2024-08-30 09:51:10 -0400

    Add files via upload

---
+++ 01_03_infinite_skills.py
@@ -0,0 +1,27 @@
+# I want to gain a lot of skills. If I had infinite skills,
+# I would make my personal creations and bring them to life.
+# I really want to make a music program because I've been pretty vested in it.
+# I honestly would love to make a digital art program because I've been doing this for 3 years.
+# And lastly I'll make a anime program because it would be about talking on the plot and story.
+
+git config --global user.email "monta@LAPTOP-EUSE7EJ6"
+git config --global user.name "MINGW64"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
