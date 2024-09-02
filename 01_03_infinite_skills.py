b1bba415121ca542cb2b5fe4c046d2ced839cc5a
Quentin Montanez <121777449+OmniVegito@users.noreply.github.com>
2024-09-02 10:16:02 -0400

    Update 01_03_infinite_skills.py

--- 01_03_infinite_skills.py
+++ 01_03_infinite_skills.py
@@ -2,10 +2,10 @@
 # I would make my personal creations and bring them to life.
 # I really want to make a music program because I've been pretty vested in it.
 # I honestly would love to make a digital art program because I've been doing this for 3 years.
-# And lastly I'll make a anime program because it would be about talking on the plot and story.
-
-git config --global user.email "monta@LAPTOP-EUSE7EJ6"
-git config --global user.name "MINGW64"
+# Next I'll make a anime program because it would be about talking on the plot and story.
+# I've been making digital art for a few years as I have a lot of experience in creation.
+# I have a lot of determination within my skills and I always strive to keep going.
+# But I also manage to adapt to new surroundings faster than normal people can.
 
 
 
